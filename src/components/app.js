0e03a7be5964dea87349aa6b6ba1658e77b19911
YutaKiyama <albero.montagna@gmail.com>
2016-07-14 11:31:14 +0900

    60 Building a List Container

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Component } from 'react';
+import {Component} from 'react';
+
 import SearchBar from '../containers/seach_bar';
+import WeatherList from '../containers/weather_list';
 
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <SearchBar />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <WeatherList />
+            </div>
+        );
+    }
 }
